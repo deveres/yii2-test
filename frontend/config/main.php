2ad46c75739314ad500e129cf451e66de042dc0d
vadim <vadimbirsan@gmail.com>
2015-11-13 13:40:38 +0300

    First manipulation. Clear install and SefURL

---
+++ frontend/config/main.php
@@ -0,0 +1,49 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'homeUrl' => '/',
+    'components' => [
+      'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'baseUrl' => '/',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                // global rule
+                // ModuleID/ControllerID/ActionID
+                '<action:about|contact|index>' => 'site/<action>',
+               
+            ]
+        ],
+        'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'request' => [
+            'baseUrl' => '/',
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
